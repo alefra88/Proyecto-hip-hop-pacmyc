16b5ab2e6f19c90229f939b1981f074eb07141af
alefra88 <efra.rlz@gmail.com>
2022-12-30 00:55:38 -0600

    proyecto-hip-hop-chlp-first-commit

---
+++ src/script.js
@@ -0,0 +1,19 @@
+function toggleVideo() {
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    trailer.classList.toggle('active');
+
+    video.currentTime = 0;
+    video.pause();
+}
+
+const fulImgBox = document.getElementById("fulImgBox"), fulImg = document.getElementById("fulImg");
+
+
+function openFulImg(reference) {
+    fulImgBox.style.display= "flex";
+    fulImg.src = reference
+}
+function closeImg() {
+    fulImgBox.style.display = "none";
+}
